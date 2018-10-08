eca28bd30ae82e3acf191210dc18cfa5676da0d2
tcew <tcew@nr028.cluster>
2018-10-08 14:30:46 -0400

    monte carlo pi sampling code for lecture on strong and weak scaling

---
+++ L14/mpi.c
@@ -0,0 +1,65 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+int main(int argc, char **argv){
+
+  long long int Ninside = 0; // number of random points inside 1/4 circle
+  long long int Ntests = 1000000000;
+  long long int n;
+  int rank, size;
+
+  double estpi = 0;
+
+  MPI_Init(&argc, &argv);
+  
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  
+  srand48(rank);
+  
+
+  double tic = MPI_Wtime();
+
+  for(n=0;n<Ntests;++n){
+    double x = drand48();
+    double y = drand48();
+    
+    if(x*x+y*y<1){
+      ++Ninside;
+    }
+  }
+
+
+  {
+    int messageLength = 1;
+    int messageDest = 0;
+
+    long long int *messageOut 
+      = (long long int*) calloc(messageLength, sizeof(long long int));
+
+    long long int *messageIn 
+      = (long long int*) calloc(messageLength, sizeof(long long int));
+
+    messageOut[0] = Ninside;
+
+    MPI_Reduce(messageOut, messageIn, messageLength, MPI_LONG_LONG_INT, MPI_SUM, messageDest, MPI_COMM_WORLD);
+    
+    if(rank==messageDest){
+      
+      estpi = 4.*(messageIn[0]/(double)(size*Ntests)); // total number of tests = size * Ntests per rank
+    }
+  }
+
+  double toc = MPI_Wtime();
+  
+  if(rank==0){
+    printf("estPi = %17.15lf\n", estpi);
+    printf("size = %d elapsed = %lg\n", size, toc-tic);
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
