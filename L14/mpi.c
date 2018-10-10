03724005b95571c97d916aac979b349805380a2d
tcew <tcew@nrlogin1.cluster>
2018-10-10 13:58:16 -0400

    implementation of parallel monte carlo for computing pi

--- L14/mpi.c
+++ L14/mpi.c
@@ -17,13 +17,16 @@ int main(int argc, char **argv){
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+  // divide tests among size processes
   Ntests = Ntests/size;
   
+  // each rank seeds the drand48 random generator differently
   srand48(rank);
-  
 
+  // start timing
   double tic = MPI_Wtime();
 
+  // perform tests on this rank
   for(n=0;n<Ntests;++n){
     double x = drand48();
     double y = drand48();
@@ -32,8 +35,9 @@ int main(int argc, char **argv){
       ++Ninside;
     }
   }
-
-
+  
+  // all ranks contribute their value of Ninside to a global sum 
+  // with result ending on rank = messageDest = 0
   {
     int messageLength = 1;
     int messageDest = 0;
@@ -47,16 +51,16 @@ int main(int argc, char **argv){
     messageOut[0] = Ninside;
 
     MPI_Reduce(messageOut, messageIn, messageLength, MPI_LONG_LONG_INT, MPI_SUM, messageDest, MPI_COMM_WORLD);
-    
+
+    // only rank == messageDest finalizes the sum
     if(rank==messageDest){
-      
       estpi = 4.*(messageIn[0]/(double)(size*Ntests)); // total number of tests = size * Ntests per rank
     }
   }
 
   double toc = MPI_Wtime();
 
-  
+  // only rank zero prints out estimate for pi
   if(rank==0){
     //    printf("estPi = %17.15lf\n", estpi);
     printf("%d %lg \n", size, toc-tic);
