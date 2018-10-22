922a1c383c50b35a7dc717294d1bc418cde9a47d
Tim Warburton <tim.warburton@gmail.com>
2018-10-22 14:23:57 -0400

    simplified examples of shared and private variables

--- L18/ompShared.c
+++ L18/ompShared.c
@@ -5,26 +5,34 @@
 #include <omp.h>
 
 int main(int argc, char **argv){
-  int threadCount = 25;
+  int threadCount = 10;
 
   omp_set_num_threads(threadCount);
 
   // still serial here
-  int i = 6;
-  int j = 5;
+  int k = 0;
   
   // fork the program
-#pragma omp parallel firstprivate(i) private(j)
+#pragma omp parallel 
   {
     // stuff in this scope gets executed by all OpenMP threads
     int rank = omp_get_thread_num();
     int size = omp_get_num_threads();
 
-    printf("thread %d out of %d threads reports: i=%d, j=%d\n",
-	   rank, size, i, j);
+    int i = k;
+    
+    printf("before: thread %d out of %d threads reports: k=%d\n",
+	   rank, size, i);
 
+    k = i+1;
+
+    printf("after: thread %d out of %d threads reports: k=%d\n",
+	   rank, size, k);
+    
   }
 
+  printf("final k = %d\n", k);
+  
   exit(0);
   return 0;
 
